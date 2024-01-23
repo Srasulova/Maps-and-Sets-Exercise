a633a2d771210aae23b04c3a496904b473baeab0
Srasulova <rasulova_sabina@yahoo.com>
2024-01-22 20:43:19 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// new Set([1,1,2,2,3,4])
+// returns Set {1, 2, 3, 4}
+
+[...new Set("referee")].join("")
+// retured "ref"
+
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1,2,3], false);
+console.log(m)
+// it returns 
+// new Map([
+//     [
+//         [
+//             1,
+//             2,
+//             3
+//         ],
+//         true
+//     ],
+//     [
+//         [
+//             1,
+//             2,
+//             3
+//         ],
+//         false
+//     ]
+// ])
+
+
+const hasDuplicate = arr => new Set(arr).size !== arr.length
+
+
+
+
+

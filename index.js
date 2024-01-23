fa226be1c69247b94ce1a534c310170961c72dd9
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-01-23 07:47:47 -0500

    finished exercise

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // new Set([1,1,2,2,3,4])
 // returns Set {1, 2, 3, 4}
 
-[...new Set("referee")].join("")
+[...new Set("referee")].join("");
 // retured "ref"
 
 let m = new Map();
-m.set([1,2,3], true);
-m.set([1,2,3], false);
-console.log(m)
-// it returns 
+m.set([1, 2, 3], true);
+m.set([1, 2, 3], false);
+console.log(m);
+// it returns
 // new Map([
 //     [
 //         [
@@ -28,13 +28,38 @@ console.log(m)
 //     ]
 // ])
 
+const hasDuplicate = (arr) => new Set(arr).size !== arr.length;
 
-const hasDuplicate = arr => new Set(arr).size !== arr.length
+// const vowelCount = (str) => {
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   let vowelsAmount = 0;
+//   for (let char of str) {
+//     if (vowels.includes(char)) {
+//       vowelsAmount++;
+//     }
+//   }
+//   const vowelsObj = new Map();
+// };
 
+// console.log(vowelCount("sabina"));
 
-const vowelCount = (str) => {
-    let vowels = "aeiou";
-    
+function isVowel(char) {
+  return "aeiuo".includes(char);
 }
 
+function vowelCount(str) {
+  const vowelMap = new Map();
+  for (let char of str) {
+    let lowerCaseChar = char.toLowerCase();
+    if (isVowel(lowerCaseChar)) {
+      if (vowelMap.has(lowerCaseChar)) {
+        vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+      } else {
+        vowelMap.set(lowerCaseChar, 1);
+      }
+    }
+  }
+  return vowelMap;
+}
 
+console.log(vowelCount("sabina"));
